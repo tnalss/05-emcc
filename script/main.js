d68dd0c6a97ee34c3f34d6f87aa58a6e1835a5c3
tnalss <indoubt@naver.com>
2022-09-22 12:56:45 +0900

    item-details popup jquery finished

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
-/**
- * 
- */
- $(function(){
-	
+$(function() {
+	$("#detail").click(function() {
+		$(".item-detail").removeClass("d-none");
+		//.show("fast";)
+	})
+
+	$("#close").click(function() {
+		$(".item-detail").addClass("d-none");
+	})
 })
