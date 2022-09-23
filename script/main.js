bf9acb738fc728846b091678bea324f782b3ddea
tnalss <indoubt@naver.com>
2022-09-23 12:51:43 +0900

    done

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+/*jQuery에서
 $(function() {
 	$("#detail").click(function() {
 		$(".item-detail").removeClass("d-none");
@@ -7,4 +8,38 @@ $(function() {
 	$("#close").click(function() {
 		$(".item-detail").addClass("d-none");
 	})
+})
+*/
+
+
+/* 자바스크립트에서 */
+window.addEventListener("DOMContentLoaded", function() {
+
+	const detailOpen = document.querySelector("#detail");
+	const detailClose = document.querySelector("#close");
+	const displayArea = document.querySelector(".item-detail");
+
+	detailOpen.addEventListener("click", function() {
+		displayArea.classList.remove('d-none');
+	})
+	detailClose.addEventListener("click", function() {
+		displayArea.classList.add('d-none');
+	})
+	
+	const smallImg= document.querySelectorAll(".small-img li");
+	const img1=document.querySelector("#img1");
+	
+	smallImg[0].addEventListener("click",function(){
+		img1.setAttribute("src","images/items1.png");
+	})
+	smallImg[1].addEventListener("click",function(){
+		img1.setAttribute("src","images/items2.png");
+	})
+	smallImg[2].addEventListener("click",function(){
+		img1.setAttribute("src","images/items3.png");
+	})
+	
+
+
+
 })
